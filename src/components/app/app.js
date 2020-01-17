d9c727dc8e14d965745b48d343f15ddf4fc9b698
CPPtrueCoder <andierodin@gmail.com>
2020-01-18 02:35:41 +0300

    Replace working with API to DidMount,Updated App component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+
+import SwapiService from "../../services/swapi-service";
+
 import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 
 import './app.css';
+import PersonDetails from "../person-details";
 
-const App = () => {
-    return (
-        <div>
-            <Header />
-            <RandomPlanet />
-
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList />
+export default class App extends Component {
+    
+    swapiService = new SwapiService();
+    
+    state = {
+        showRandomPlanet: true,
+        selectedPerson:null
+    };
+    
+    toggleRandomPlanet = () => {
+        this.setState((state) => {
+            return {
+                showRandomPlanet: !state.showRandomPlanet
+            }
+        });
+    };
+    onPersonSelected=(id)=>{
+     this.setState({
+         selectedPerson:id
+     })
+    };
+    
+    
+    render() {
+        
+        const planet = this.state.showRandomPlanet ?
+            <RandomPlanet/> :
+            null;
+      
+        return (
+       
+                <div className="stardb-app">
+                    <Header />
+                    {planet}
+                <button className="toggle-planet btn btn-warning btn-lg mb-4" onClick={this.toggleRandomPlanet}>Toogle Random</button>
+                    <div className="row mb-2">
+                        <div className="col-md-6">
+                            <ItemList onItemSelected={this.onPersonSelected}/>
+                        </div>
+                        <div  className="col-md-6">
+                            <PersonDetails personId={this.state.selectedPerson}/>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-6">
-                    <PersonDetails />
-                </div>
-            </div>
-        </div>
-    );
-};
-
-export default App;
+                
+          
+        );
+    }
+}
